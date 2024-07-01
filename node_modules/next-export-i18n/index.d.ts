d635123cad9574f049d561ae23f5974d0e884a86
issue-truck <13akstjq.blog19@gmail.com>
2024-07-01 23:56:02 +0900

    add post

--- node_modules/next-export-i18n/index.d.ts
+++
@@ -1,9 +0,0 @@
-import useLanguageQuery from "./hooks/use-language-query";
-import useSelectedLanguage from "./hooks/use-selected-language";
-import useLanguageSwitcherIsActive from "./hooks/use-language-switcher-is-active";
-import { useTranslation } from "./hooks/use-translation";
-import LanguageSwitcher from "./components/language-switcher";
-import LinkWithLocale from "./components/link-with-locale";
-import i18n from "./index.js";
-export { useLanguageQuery, useSelectedLanguage, useLanguageSwitcherIsActive, useTranslation, LanguageSwitcher, LinkWithLocale, };
-export default i18n;
